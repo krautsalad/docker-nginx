55e9f519f8a3bccb614b0f6d422e6d1d458d44eb
Sebastian Helbig <sebastian.helbig@krautsalad.com>
2025-03-30 15:28:15 +0200

    dockerignore

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-mkdir -p /etc/nginx/acme-challenge
 ln -sf /usr/local/modsecurity/lib/libmodsecurity.so.${MODSEC3_VERSION} /usr/local/modsecurity/lib/libmodsecurity.so.3.0;
 ln -sf /usr/local/modsecurity/lib/libmodsecurity.so.${MODSEC3_VERSION} /usr/local/modsecurity/lib/libmodsecurity.so.3;
 ln -sf /usr/local/modsecurity/lib/libmodsecurity.so.${MODSEC3_VERSION} /usr/local/modsecurity/lib/libmodsecurity.so;
